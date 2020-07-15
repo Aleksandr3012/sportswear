2e5b94153046b35b37389a23108fd3944f2d66d8
Pavel <wol1414@gmail.com>
2020-07-15 17:50:03 +0300

    done catagory

--- public/js/common.js
+++ public/js/common.js
@@ -155,10 +155,10 @@ function eventHandler() {
 				$('.top-nav  ').removeClass('fixed');
 			}
 		}); // конец добавил
+	}
 
-		if (window.matchMedia("(min-width: 992px)").matches) {
-			JSCCommon.closeMenu();
-		}
+	if (window.matchMedia("(min-width: 992px)").matches) {
+		$(".main-categories-wrap:first-child, .main-nav__nav ul:first-child li:first-child").addClass('active');
 	}
 
 	$(window).resize(function () {
@@ -303,15 +303,32 @@ function eventHandler() {
 			}
 		}
 	}));
-	$(".main-nav [data-tab]").hover(function () {
+	$(".main-nav [data-tab] a").hover(function () {
+		var dataTab = $(this).parent().data('tab');
+		$('[data-tab]').removeClass('active');
+		$(this).parent().addClass('active').siblings().removeClass('active');
+		$(dataTab).addClass('active').siblings().removeClass('active');
+		return false;
+	});
+	$(".main-nav [data-tab]").click(function () {
 		var dataTab = $(this).data('tab');
 		$('[data-tab]').removeClass('active');
 		$(this).addClass('active').siblings().removeClass('active');
 		$(dataTab).addClass('active').siblings().removeClass('active');
+		return false;
 	});
 	$(".toggle-main-menu--js").click(function () {
 		$(this).toggleClass('on');
 		$(".main-nav").toggle();
+		$("body").toggleClass('fixed');
+	});
+	$(" .toggle-menu-mobile--js").click(function () {
+		$('.toggle-main-menu--js').removeClass('on');
+		$(".main-nav").hide();
+		$("body").removeClass('fixed');
+	});
+	$(".toggle-menu-mobile--inner-js").click(function () {
+		$(this).parents(".main-categories-wrap").removeClass('active');
 	});
 	var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
 
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -169,11 +169,12 @@ function eventHandler() {
 			}
 		});
 		// конец добавил
-		if (window.matchMedia("(min-width: 992px)").matches) {
-			JSCCommon.closeMenu();
-		}
+		
 	}
+	if (window.matchMedia("(min-width: 992px)").matches) {
+		$(".main-categories-wrap:first-child, .main-nav__nav ul:first-child li:first-child").addClass('active')
 
+	}
 	$(window).resize(function () {
 		heightses();
 
@@ -359,18 +360,42 @@ function eventHandler() {
 		 
 	});
 
-	$(".main-nav [data-tab]").hover(function () {
+ 
+	$(".main-nav [data-tab] a").hover(function () {
+		var dataTab = $(this).parent().data('tab');
+		
+		$('[data-tab]').removeClass('active');
+		$(this).parent().addClass('active').siblings().removeClass('active');
+		$(dataTab).addClass('active').siblings().removeClass('active');
+		return false;
+	})
+	
+	$(".main-nav [data-tab]").click(function () {
 		var dataTab = $(this).data('tab');
-		 
+		
 		$('[data-tab]').removeClass('active');
 		$(this).addClass('active').siblings().removeClass('active');
 		$(dataTab).addClass('active').siblings().removeClass('active');
+		return false;
 	})
+	
 	$(".toggle-main-menu--js").click(function () {
 		$(this).toggleClass('on');
 		$(".main-nav").toggle();
+		$("body").toggleClass('fixed')
 	})
-
+	
+	$(" .toggle-menu-mobile--js").click(function () {
+		$('.toggle-main-menu--js').removeClass('on');
+		$(".main-nav").hide();
+		$("body").removeClass('fixed')
+	})
+	
+	$(".toggle-menu-mobile--inner-js").click(function () {
+		$(this).parents(".main-categories-wrap").removeClass('active');
+	})
+	
+	
 	var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
 	if (isIE11) {
 		$("body").prepend(`<p   class="browsehappy container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p>`)
